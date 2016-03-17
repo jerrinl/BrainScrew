a7da8ffc864081ba65c1351900586716f6deab70
bellok <kbello_19@yahoo.com>
2016-03-17 01:08:35 -1000

    Complete splash screen and set background and default text color

--- BrainScrew/app/src/main/java/com/ics466/brainscrew/SplashActivity.java
+++ BrainScrew/app/src/main/java/com/ics466/brainscrew/SplashActivity.java
@@ -1,35 +1,36 @@
 package com.ics466.brainscrew;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().hide();
-        setContentView(R.layout.activity_splash);
+//        getActionBar().hide();
+//        setContentView(R.layout.activity_splash);
 
-        Thread welcomeThread = new Thread() {
 
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    super.run();
-                    sleep(10000);  //Delay of 10 seconds
-                } catch (Exception e) {
-
-                } finally {
-
-                    Intent i = new Intent(SplashActivity.this,
-                            MainActivity.class);
-                    startActivity(i);
-                    finish();
-                }
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
-        };
-        welcomeThread.start();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
