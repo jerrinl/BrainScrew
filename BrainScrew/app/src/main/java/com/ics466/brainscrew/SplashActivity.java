0ee7ad879dd966232384953a205a58b107194f12
bellok <kbello_19@yahoo.com>
2016-03-16 20:41:04 -1000

    Add empty loading screen

---
+++ BrainScrew/app/src/main/java/com/ics466/brainscrew/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.ics466.brainscrew;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().hide();
+        setContentView(R.layout.activity_splash);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(10000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
