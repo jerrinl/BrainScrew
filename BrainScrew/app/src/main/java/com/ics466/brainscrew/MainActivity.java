b42b7b616ae0bde87e7519117b5b7dc25f69594b
bellok <kbello_19@yahoo.com>
2016-04-12 16:21:40 -1000

    Start to add click sound effect

--- BrainScrew/app/src/main/java/com/ics466/brainscrew/MainActivity.java
+++ BrainScrew/app/src/main/java/com/ics466/brainscrew/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ics466.brainscrew;
 
 import android.content.Intent;
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * MainActivity is for the Home Screen where the user can navigate to the level select,
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.click);
+        ImageView startClick = (ImageView) this.findViewById(R.id.play);
+
+        startClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
     }
 
     /**
